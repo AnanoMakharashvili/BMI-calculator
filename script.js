05cbcfa7884b7cb3604a5e31de13938ba1117b52
Anano Makharashvili <ananomakharashvili@gmail.com>
2024-06-21 18:48:35 +0400

    corrected

--- script.js
+++ script.js
@@ -3,19 +3,50 @@ const weightInput = document.getElementById("weight-input");
 const getBmiResult = document.getElementById("num-bmi-style");
 const welcomeButton = document.getElementById("welcome-style");
 const getHealthyWeight = document.getElementById("bmi-suggest");
+const getMetricButton = document.getElementById("metric-name");
+const getImperialRadio = document.getElementById("imperial");
+const getMetricRadio = document.getElementById("metric");
+const bmiLbsElement = document.getElementById(".below-box-style ");
+const heightElement = document.getElementById("height-ft-input");
+const weightElement = document.getElementById("weight-st-input");
+const bmiResult = document.getElementById("bmi-lbs");
+const getHealthy = document.getElementById("healthy-weight-bmi-style");
 
 let heightValue = 0;
 let weightValue = 0;
+let imperialValue = 0;
+let heightAmount = 0;
+let weightAmount = 0;
 
 heightInput.addEventListener("input", (event) => {
   heightValue = Number(event.target.value);
   calculateBmi();
 });
 
+heightElement.addEventListener("input", (event) => {
+  heightAmount = Number(event.target.value);
+  calculateBmiElement();
+});
+
 weightInput.addEventListener("input", (event) => {
   weightValue = Number(event.target.value);
   calculateBmi();
 });
+
+weightElement.addEventListener("input", (event) => {
+  weightAmount = Number(event.target.value);
+  calculateBmiElement();
+});
+
+if (getMetricRadio.checked) {
+  getMetricRadio.checked = false;
+  getImperialRadio.checked = true;
+} else if (getImperialRadio.checked) {
+  getImperialRadio.checked = false;
+  getMetricRadio.checked = true;
+} else {
+  getMetricRadio.checked = true;
+}
 
 const calculateBmi = () => {
   const dividedHeight = heightValue / 100;
@@ -26,4 +57,10 @@ const calculateBmi = () => {
   } else {
     welcomeButton.style.display = "none";
   }
+};
+
+const calculateBmiElement = () => {
+  const dividedHeight = heightAmount / 100;
+  const bmi = weightAmount / (dividedHeight * dividedHeight);
+  bmiResult.textContent = bmi.toFixed(1);
 };
